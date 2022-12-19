eddcfa44627eb157b4245d2dac51c99d99ee9595
TheJeffRicardo <konkemalindi@gmail.com>
2022-12-19 14:23:05 +0200

    'spinner'

---
+++ src/components/Product.vue
@@ -0,0 +1,71 @@
+<template>
+    <div v-if="products.length6">
+        <div v-for="item in products" :key="item">
+            <div class="m-3">
+                <div class="">
+                    <div class="card text-bg-dark">
+                        <div class="card-body">
+                            <h4 class="card-title">Name: {{item.name}}</h4>
+                            <h5 class="card-title">Category: {{item.category}}</h5>
+                            <p class="card-text">Value: R{{item.value}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-else>
+        <Spinners></Spinners>
+    </div>
+</template>
+<script>
+import Spinners from '@/components/Spinner.vue'
+export default {
+    name: 'product',
+    data() {
+        return {
+            products: [
+//     {
+//         id: 1,
+//         name: "Carrot",
+//         category: "Veg",
+//         value: 1020,
+//     },
+//     {
+//         id: 2,
+//         name: "Pumpkin",
+//         category: "Veg",
+//         value: 1500,
+//     },
+//     {
+//         id: 3,
+//         name: "Egg Plant",
+//         category: "Veg",
+//         value: 1350,
+//     },
+//    {
+//         id: 4,
+//         name: "Cabbage",
+//         category: "Veg",
+//         value: 600,
+//     },{
+//         id: 5,
+//         name: "Broccoli",
+//         category: "Veg",
+//         value: 100,
+//     },
+//     {
+//         id: 6,
+//         name: "Onions",
+//         category: "Veg",
+//         value: 470,
+//     }
+]}},
+    components:{
+        Spinners
+    }
+}
+</script>
+<style>
+    
+</style>
---
+++ src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center">
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden"></span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'spinner'
+}
+</script>
+<style>
+    
+</style>
